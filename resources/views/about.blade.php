caa52eae6260695c6d60ba2525257740f4c4e797
elfalbirri <61033380+elfalbirri@users.noreply.github.com>
2021-10-10 18:45:34 +0700

    menambahkan halaman about
    tadi salah letak folder

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman About</h1>
+    <div class="container mt-5">
+    <h3>About Us</h3>
+
+    <div class="row">
+        <div class="col-5">
+            <img src="img/about.jpg" width=450>
+        </div>
+        <div class="col-6">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nobis obcaecati quia animi accusantium? Consequatur similique dolorem aperiam illum soluta veniam asperiores debitis aspernatur modi facilis dicta doloribus dignissimos praesentium repellat animi iure, delectus aut officiis eum explicabo? Fuga, tenetur laboriosam recusandae ut aut consequuntur labore dolor ab placeat officia?</p>
+            </br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, laborum quam dolore maiores minus delectus quidem ab corrupti similique! Non soluta odit praesentium fuga cupiditate? Lorem ipsum dolor sit amet consectetur adipisicing elito.</p>
+        </div>
+    </div>
+
+    <div class="row text-center mt-5">
+        <div class="col">
+            <h3>Our Staff</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4 text-center">
+            <img src="img/profile3.jpg" alt="Maulidya" width=200> 
+            <p>Maulidya Rista Yuniar</p>
+        </div>
+        <div class="col-4 text-center">
+            <img src="img/profile2.jpg" alt="Roudhotul" width=200> 
+            <p>Roudhotul Jannah</p>
+        </div>
+        <div class="col-4 text-center">
+            <img src="img/profile1.jpg" alt="Rifqa" width=200> 
+            <p>Rifqa A Aliyyah</p>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-4 text-center">
+            <img src="img/profile4.jpg" alt="Arizfi" width=200>
+            <p>Arizfi Agustina</p>
+        </div>
+        <div class="col-4 text-center">
+            <img src="img/profile5.jpg" alt="Elfal" width=200>
+            <p>Elfal Birri Firamadhani</p>
+        </div>
+    </div>
+
+</div>
  @endsection
